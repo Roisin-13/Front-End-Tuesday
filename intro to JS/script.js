d4c7847fcb21108389a3210d903d9fc15f7f8efe
Roisin-13 <roisin.wheeler@gmail.com>
2021-10-12 10:17:05 +0100

    js console.log added, and styled

---
+++ intro to JS/script.js
@@ -0,0 +1,7 @@
+let fName = "Roisin ";
+let sName = "Wheeler ";
+let home = "London ";
+let star = "Sagittarius ";
+console.log(fName + sName + home + star);
+
+console.log("%c" + fName, "color: orange; font-family: fantasy; font-weight: bold; background-color: black; padding: 10px;");
